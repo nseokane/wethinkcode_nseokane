b083a337df5dc2c9b77376160f31b05cfb2401f8
nseokane <nseokane@student.wethinkcode.co.za>
2020-06-17 15:27:05 +0200

    commiting pushing_to_git.sh bash file for pushing files remotely

--- pushing_to_git.sh
+++ pushing_to_git.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 clear
+
 git status
 echo "What do you want to add to the commiting stage?"
 read -p 'name:' name
 git add $name
 git status
 sleep 3
+
 echo "commit description?"
 read -p 'description:' description
-git commit -m "$decription"
+git commit -m "$description"
 git status
 sleep 3
+
 git push origin master
