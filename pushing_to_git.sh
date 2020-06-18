fffd32e766c58b7626244a4e7b7617f450960cce
nseokane <nseokane@student.wethinkcode.co.za>
2020-06-18 22:04:07 +0200

    commiting the pushing_to_git.sh file

--- pushing_to_git.sh
+++ pushing_to_git.sh
@@ -5,11 +5,11 @@ git status
 echo "What do you want to add to the commiting stage?"
 read -p 'name:' name
 git add $name
-git status
 
+git status
 echo "commit description?"
 read -p 'description:' description
 git commit -m "$description"
-git status
 
+git status
 git push -u origin master
