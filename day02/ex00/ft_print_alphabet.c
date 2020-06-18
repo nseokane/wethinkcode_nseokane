81f49675083938fa6aab3ab756927fe19e26cdbf
nseokane <nseokane@student.wethinkcode.co.za>
2020-06-18 22:00:06 +0200

    comming day02/ex00 directory

---
+++ day02/ex00/ft_print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void ft_putchar(char a)
+{
+    write(1, &a, 1);
+}
+
+int ft_print_alphabet()
+{
+    char letter = 'a';
+    while (letter <= 'z')
+    {
+        ft_putchar(letter);
+        letter++;
+    }
+    write(1, "\n", 2);
+    return 0;
+}
