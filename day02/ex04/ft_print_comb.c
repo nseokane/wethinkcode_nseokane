b16cf37c14930e8f5ac856c4915916370d0bfd10
nseokane <nseokane@student.wethinkcode.co.za>
2020-06-19 13:10:00 +0200

    commiting incomplete day02/ex04 file

---
+++ day02/ex04/ft_print_comb.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nseokane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 11:01:53 by nseokane          #+#    #+#             */
+/*   Updated: 2020/06/19 11:50:04 by nseokane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <stdio.h>
+void ft_putchar(){}
+
+
+void ft_combine(char a, char b, char c){
+	printf("%d%d%d\n", a, b, c);
+	// write(1, num, 4);
+}
+
+void ft_print_comb(void){
+	int a;
+	while (a <= 9){
+		int b;
+		b = a + 1;
+		while (b <= 9){
+			int c;
+			c = b + 1;
+			while (c <= 9){
+				ft_combine(a, b, c);
+				c++;
+			}
+			b++;
+		}		
+		a++;
+	}
+}
+
+int main(){
+	ft_print_comb();
+	return 0;
+}
