8816a1474842a54e4c894bb83938032b9372a21a
nseokaneJHB <nseokane@student.wethinkcode.co.za>
2020-06-23 10:07:07 +0200

    commiting changes to day04/ex00

--- day04/ex00/ft_iterative_factorial.c
+++ day04/ex00/ft_iterative_factorial.c
@@ -1,15 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nseokane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 10:05:35 by nseokane          #+#    #+#             */
+/*   Updated: 2020/06/23 10:05:39 by nseokane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
-int main() {
-    char fact = ft_recursive_factorial(5);
-    printf("Answer %d", fact);
-    return 0;
-}
 
 int ft_recursive_factorial(int nb){
-    int i = nb, fact = 1;
-    while (nb / i != nb) {
+    int i = 1, fact = 1;
+    while (i <= nb) {
         fact = fact * i;
-        i--;
+        i++;
     }
     return fact;
 }
