6350f6c01086e38ee47af63bcdc83871b1304886
nseokaneJHB <nseokane@student.wethinkcode.co.za>
2020-06-26 16:33:08 +0200

    commiting changes to project sastantua

--- project/ex00/sastantua.c
+++ project/ex00/sastantua.c
@@ -40,65 +40,40 @@ void ft_odd_numbers(int nb){
 	}
 }
 
+void ft_printing(int subrow, int starNo, int spaceNo){
+    int i = 0;
+    while (i < subrow){
+        ft_dash_left(spaceNo);
+        ft_odd_numbers(i + (starNo + i));
+        write(1, "\\\n", 4);
+        spaceNo--;
+        i++;
+    }
+}
 
 void ft_putchar(int row){
     int subrow, starNo, spaceNo, i = 0;
 
     if (row == 1){
-        subrow = 3;
-        starNo = 1;
-        spaceNo = subrow - 1;
-        while (i < subrow){
-            ft_dash_left(spaceNo);
-            ft_odd_numbers(i + (starNo + i));
-            write(1, "\\\n", 4);
-            spaceNo--;
-            i++;
-        }
+        ft_printing(3, 1, (3 - 1));
     }else if (row == 2){
-        subrow = 4;
-        starNo = 11;
-        spaceNo = subrow - 1;
-        while (i < subrow){
-            ft_dash_left(spaceNo);
-            ft_odd_numbers(i + (starNo + i));
-            write(1, "\\\n", 4);
-            spaceNo--;
-            i++;
-        }
+        ft_printing(3, 1, (9 - 1));
+        ft_printing(4, 11, (4 - 1));
     }else if (row == 3){
-        subrow = 5;
-        starNo = 23;
-        spaceNo = subrow - 1;
-        while (i < subrow){
-            ft_dash_left(spaceNo);
-            ft_odd_numbers(i + (starNo + i));
-            write(1, "\\\n", 4);
-            spaceNo--;
-            i++;
-        }
+        ft_printing(3, 1, (16 - 1));
+        ft_printing(4, 11, (11 - 1));
+        ft_printing(5, 23, (5 - 1));
     }else if (row == 4){
-        subrow = 6;
-        starNo = 39;
-        spaceNo = subrow - 1;
-        while (i < subrow){
-            ft_dash_left(spaceNo);
-            ft_odd_numbers(i + (starNo + i));
-            write(1, "\\\n", 4);
-            spaceNo--;
-            i++;
-        }
+        ft_printing(3, 1, (24 - 1));
+        ft_printing(4, 11, (19 - 1));
+        ft_printing(5, 23, (13 - 1));
+        ft_printing(6, 39, (6 - 1));
     }else if (row == 5){
-        subrow = 7;
-        starNo = 57;
-        spaceNo = subrow - 1;
-        while (i < subrow){
-            ft_dash_left(spaceNo);
-            ft_odd_numbers(i + (starNo + i));
-            write(1, "\\\n", 4);
-            spaceNo--;
-            i++;
-        }
+        ft_printing(3, 1, (35 - 1));
+        ft_printing(4, 11, (30 - 1));
+        ft_printing(5, 23, (24 - 1));
+        ft_printing(6, 39, (16 - 1));
+        ft_printing(7, 57, (7 - 1));
     }
     
 }
