7b3b59a8d7b63273cbd421be008ac2ba76d5c131
nseokaneJHB <nseokane@student.wethinkcode.co.za>
2020-06-26 11:41:25 +0200

    commiting day06/ex01

--- day06/ex01/ft_print_program_name.c
+++ day06/ex01/ft_print_program_name.c
@@ -3,31 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_program_name.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sdubazan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nseokane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/23 13:41:47 by sdubazan          #+#    #+#             */
-/*   Updated: 2020/06/23 13:42:38 by sdubazan         ###   ########.fr       */
+/*   Created: 2020/06/25 15:41:58 by nseokane          #+#    #+#             */
+/*   Updated: 2020/06/25 15:42:00 by nseokane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putchar(char c);
+#include <unistd.h>
 
-void ft_putstr(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	(void)argc;
-	ft_putstr(argv[0]);
-	ft_putchar('\n');
-	return (0);
+void ft_putchar(char *b){
+	write(1, b, sizeof b);
 }
